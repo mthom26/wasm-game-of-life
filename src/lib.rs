d7c05e40f220f7a8f765089de5d5c6088ea341ec
mthom26 <mthom2626@gmail.com>
2019-01-22 15:07:49 +0000

    Initial commit
    Initial commit with basic working universe

---
+++ src/lib.rs
@@ -0,0 +1,146 @@
+extern crate cfg_if;
+extern crate wasm_bindgen;
+
+mod utils;
+
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+
+use std::fmt;
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_column in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_column == 0 && delta_row == 0 {
+                    // This is the current cell so skip it
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_column = (column + delta_column) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_column);
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 3 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            }) 
+            .collect();
+        
+        Universe {
+            width,
+            height, 
+            cells
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn get_width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn get_height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn get_cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+
+                let live_neighbours = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, live_neighbours) {
+                    // Live Cell with less than 2 neighbors
+                    (Cell::Alive, n) if n < 2 => Cell::Dead,
+                    // Live Cell with 2 or 3 neighbors
+                    (Cell::Alive, 2) => Cell::Alive,
+                    (Cell::Alive, 3) => Cell::Alive,
+                    // Live Cell with more than 3 neigbors
+                    (Cell::Alive, n) if n > 3 => Cell::Dead,
+                    // Dead Cell with 3 neighbors
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // Otherwise dead cell stays dead
+                    (cell_state, _) => cell_state  
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = match cell {
+                    Cell::Alive => '◼',
+                    Cell::Dead => '◻'
+                };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
+}
